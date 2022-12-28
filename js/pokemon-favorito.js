161bec005266e4b2465ef219a8aafb443b6029a2
Shirley Azurin <wheeloftheyear159@gmail.com>
2022-12-28 02:47:40 -0500

    Avance de la función favoritos

---
+++ js/pokemon-favorito.js
@@ -0,0 +1,73 @@
+// Botón agregar a favoritos Listo
+
+const btn_add_favourites = document.getElementById('btn_add_favourites');
+
+btn_add_favourites.addEventListener('click', confirmAddFavourites)
+
+
+// Debe solicitar la confirmación de usuario
+function confirmAddFavourites() {
+    if(confirm('está usted seguro?')) { // Si le da a aceptar llama a la función addFavourites
+        addFavourites()
+    } else {
+        console.log('vuelva pronto')
+    }
+}
+
+// Obtenemos los datos en un object
+function addFavourites() {
+    const pokemonName = pokemon.value; // Obtiene el value del option seleccionado
+    const btn_favorito = document.getElementById(`${pokemonName}`); 
+    
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`, {
+        method: 'get'
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        let userTemp = JSON.parse(localStorage.getItem(usuario[0].uid))
+
+        const img = data.sprites.front_default;
+        const name = data.name;
+        const type = data.types[0].type.name;
+        
+        let pokeData = { // Este objeto debería enviarse a la base de datos
+            name: name,
+            img: img,
+            type: type
+        }
+
+
+        console.log(pokeData);
+        console.log(userTemp)
+
+        sendFavourites(pokeData)
+
+        
+        
+    });
+}
+
+function sendFavourites(pokeData) {
+    fetch('https://backapipoke-production.up.railway.app/api/usuarios', {
+        method: 'POST',
+        body: JSON.stringify(pokeData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+}
+
+
+
+// Agrega los datos del pokemon a la base de datos
+
+
+
+// El background del nombre del pokemon debe cambiar
