c5ba8165ed3e3c7af7c175119e81c4ccfaf4a9a4
Shirley Azurin <wheeloftheyear159@gmail.com>
2022-12-28 16:14:18 -0500

    Función Eliminar de Favoritos

--- js/pokemon-eliminar.js
+++ js/pokemon-eliminar.js
@@ -1,7 +1,16 @@
-const pokemon_close_btn = document.querySelector('div.pokemon')
-
-pokemon_close_btn.addEventListener('onmouseenter', delete_pokemon)
-
-function delete_pokemon() {
-    alert('funciona')
+function eliminarPokemonFavorito(pokemon_delete_button, id) {
+    pokemon_delete_button.addEventListener('click', (event) => {
+        fetch(`https://backapipoke-production.up.railway.app/api/favoritos/${id}`, { // Obtiene el id desde filteredData
+            method: 'DELETE',
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            const pokemonContainer = event.target.parentNode; // Elimina pokemon_total_container del DOM
+            pokemonContainer.remove();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    });
 }
